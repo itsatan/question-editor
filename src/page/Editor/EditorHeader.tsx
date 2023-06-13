61095e629f1bef9165a8888a68920315a9a9a30a
v_zhujiaxing01 <v_zhujiaxing01@baidu.com>
2023-06-13 13:12:17 +0800

    feat: 手动保存

--- src/page/Editor/EditorHeader.tsx
+++ src/page/Editor/EditorHeader.tsx
@@ -2,11 +2,14 @@ import React, { ChangeEvent, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button, Input, InputRef, Space, Typography } from 'antd'
 import { LeftOutlined, CheckOutlined, CloudUploadOutlined, EditOutlined } from '@ant-design/icons'
+import { useKeyPress, useRequest } from 'ahooks'
+import { useDispatch } from 'react-redux'
 import useGetPageInfo from '@/hooks/useGetPageInfo'
+import useGetComponentInfo from '@/hooks/useGetComponentInfo'
+import { ComponentInfoType } from '@/store/question'
+import { PAGE_INFO_INIT_STATE_TYPE, changePageTitle } from '@/store/pageInfo'
 import EditorToolbar from './EditorToolbar'
 import styles from './EditorHeader.module.scss'
-import { useDispatch } from 'react-redux'
-import { changePageTitle } from '@/store/pageInfo'
 
 const { Title } = Typography
 
@@ -44,7 +47,41 @@ const TitleElem: React.FC = () => {
 		</Space>
 	)
 }
-
+// 手动保存
+function manualSave(
+	componentList: Array<ComponentInfoType>,
+	pageInfo: PAGE_INFO_INIT_STATE_TYPE
+): Promise<{ componentList: Array<ComponentInfoType>; pageInfo: PAGE_INFO_INIT_STATE_TYPE }> {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve({ componentList, pageInfo })
+		}, 666)
+	})
+}
+const ManualSaveButton: React.FC = () => {
+	const { componentList } = useGetComponentInfo()
+	const pageInfo = useGetPageInfo()
+	const { loading, run: save } = useRequest(() => manualSave(componentList, pageInfo), {
+		manual: true,
+	})
+	// 快捷键手动保存
+	useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+		event.preventDefault() // 阻止默认行为
+		if (loading) return
+		save()
+	})
+	return (
+		<Button
+			type="default"
+			disabled={loading}
+			loading={loading}
+			icon={<CheckOutlined />}
+			onClick={save}
+		>
+			保存
+		</Button>
+	)
+}
 const EditorHeader: React.FC = () => {
 	const navigate = useNavigate()
 	return (
@@ -63,9 +100,7 @@ const EditorHeader: React.FC = () => {
 				</div>
 				<div className={styles.right}>
 					<Space>
-						<Button type="default" icon={<CheckOutlined />}>
-							保存
-						</Button>
+						<ManualSaveButton />
 						<Button type="primary" icon={<CloudUploadOutlined />}>
 							发布
 						</Button>
